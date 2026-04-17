c7c2bcf26e81657ba91d015b3ec1573e3b5b37be
Sumit Parmar <sumitparmar496@gmail.com>
2026-04-17 12:19:02 +0530

    live Url changes

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,7 +1,12 @@
 export const environment = {
   production: true,
-  apiBaseUrl: 'http://localhost:5000/api',
-  socketUrl: 'http://localhost:5000',
+
+  // apiBaseUrl: 'http://localhost:5000/api',
+  // socketUrl: 'http://localhost:5000',
+
+  apiBaseUrl: 'https://movekart.in/api',
+  socketUrl: 'https://movekart.in',
+
   googleMapsApiKey: 'AIzaSyBUGjt6NN3biO0jQygMcsZmWJNKm-BUrSU',
   currency: 'INR',
   gaEnabled: true,
